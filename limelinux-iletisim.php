721e7d272eddd3f3786452178004e33ea7b1403f
acarokan <acarokan55@gmail.com>
2017-01-11 00:01:59 +0300

    diğer sayfalar

---
+++ limelinux-iletisim.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+	<title>Lime Linux Hakkında</title>
+	
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+	<div class="nav"> 
+		<div class="width1200">
+			<a href="index.html"><img src="logo.png" alt="LimeLinux Logo" /></a>
+			<ul>
+				<li><a href="index.html" >Ana Sayfa</a></li>
+				<li><a href="lime-linux-hakkinda.html" >Hakkımızda</a></li>
+				<li><a href="lime-linux-ekibi-kimler.html" >Ekibimiz</a></li>
+				<li><a href="https://github.com/LimeLinux" >Github</a></li>
+				<li><a href="limelinux-indir.html" >İndir</a></li>
+				<li><a href="lime-linux-destek-olmak-icin.html" >Destek</a></li>
+				<li><a href="limelinux-iletisim.php" >İletişim</a></li>
+				<li><a href="limelinux-blog.html" >Blog</a></li>
+			</ul>	
+		</div>
+	</div>
+	
+	<div class="iletisim">
+		
+		
+			<table>
+			<form>
+				<tr>
+					<td style="text-align:center; padding-bottom:10px;" colspan="3"><h1>İletişim Formu</h1></td>
+					
+				</tr>
+				
+				<tr>
+					<td>İsim*</td>
+					<td>:</td>
+					<td><input type="text" placeholder="isminizi giriniz" required/></td>
+				</tr>
+				
+				<tr>
+					<td>Soyisim*</td>
+					<td>:</td>
+					<td><input type="text" placeholder="soyisminizi giriniz" required/></td>
+				</tr>
+				
+				<tr>
+					<td>E-mail*</td>
+					<td>:</td>
+					<td><input type="email" placeholder="E-mail adresinizi giriniz" required/></td>
+				</tr>
+				
+				<tr>
+					<td>Mesajınız*</td>
+					<td>:</td>
+					<td><textarea name="mesaj" rows="8"cols="40" placeholder="Destek almak istediğiniz konuyu açıklayınız..." required></textarea></td>
+				</tr>
+				
+				<tr>
+					<td colspan="3" style="text-align:center">
+									<input type="reset" value="Formu Temizle"/>
+									<input  type="submit" value="Gönder" />
+					</td>
+				</tr>
+				
+			</table>
+		
+		</form>
+		
+	
+	</div>
+		
+	
+	<div class="footer">
+		<div class="width1200">
+			<text style="font-weight:bold">Sosyal: </text> <a href="">Facebook</a> <a href="">Google+</a> <a href="">Twitter</a> <br /><br />
+		
+			Copyright ©2017 Lime Linux.<br />
+			All rights reserved.
+		</div>
+	</div>
+
+</body>
+</html>
